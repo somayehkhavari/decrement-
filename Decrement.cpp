a0cff0170f0be62174216ad44263f4f3f414b72f
somayeh khavari <46685906+somayehkhavari@users.noreply.github.com>
2019-01-25 15:31:21 +0330

    Add files via upload

---
+++ Decrement.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+	int decrement(int);
+		int x=7;
+			int main(){
+				x=decrement(x);
+				cout<<x;
+					x=decrement(x);
+					cout<<x;
+			}
+				int decrement(int a){
+				return a-1;
+				}
+				
